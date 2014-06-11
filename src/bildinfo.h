e5998e5b3d7f9ee4e0ec2102209bbac157e1a692
Rick <rickfrey@gmx.de>
2014-06-11 12:47:16 +0200

    Klasse

--- src/bildinfo.h
+++ src/bildinfo.h
@@ -8,13 +8,15 @@ class Bildinfo
 private:
     int x,y,z,r,p,ya;//Pose
     int Linienanzahl;
-    int Linienvektor;
+    int Linienarray;
 
 
 public:
     Bildinfo();
     ~Bildinfo();
 
+    void setPose(float x, float y, float z, float r, float p, float ya);
+    void setLinienarray();
 
 };
 
